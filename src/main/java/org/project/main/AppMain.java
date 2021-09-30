27d8a0f4d20e23bdfbd13d674d0024e2180ebd32
Fede22dev <79111932+Fede22dev@users.noreply.github.com>
2021-09-30 14:49:13 +0200

    Sistemati problemi generali e sistemata schermata login

--- src/main/java/org/project/main/AppMain.java
+++ src/main/java/org/project/main/AppMain.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
 import java.io.IOException;
 
 /**
@@ -15,28 +16,39 @@ import java.io.IOException;
 public class AppMain extends Application {
 
     private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("appmain"));
-        stage.initStyle(StageStyle.TRANSPARENT);
-        stage.setScene(scene);
-        scene.setFill(Color.TRANSPARENT);
-        stage.setResizable(false);
-        stage.show();
-    }
+    private double xOffset, yOffset;
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AppMain.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AppMain.class.getResource("fxml/" + fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("appmain"));
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
+        scene.setOnMousePressed(mouseEvent -> {
+            xOffset = mouseEvent.getSceneX();
+            yOffset = mouseEvent.getSceneY();
+        });
+
+        scene.setOnMouseDragged(mouseEvent -> {
+            stage.setX(mouseEvent.getScreenX() - xOffset);
+            stage.setY(mouseEvent.getScreenY() - yOffset);
+        });
     }
 
 }
