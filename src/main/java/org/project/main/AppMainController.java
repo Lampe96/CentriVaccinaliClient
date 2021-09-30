b007fc3241c286efe69baab5b89aacac78520b16
Fede22dev <79111932+Fede22dev@users.noreply.github.com>
2021-09-30 20:00:00 +0200

    Quasi ultimata graficamente schermata di login e aggiunte varie librerie grafiche

--- src/main/java/org/project/main/AppMainController.java
+++ src/main/java/org/project/main/AppMainController.java
@@ -1,4 +1,84 @@
 package org.project.main;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.InnerShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
 public class AppMainController {
+
+    @FXML
+    public ImageView BT_minimize;
+
+    @FXML
+    public ImageView BT_quit;
+
+    @FXML
+    public AnchorPane AP_ext;
+
+    @FXML
+    public MFXButton BT_login;
+
+    @FXML
+    public Label BT_signUp;
+
+    @FXML
+    public Label BT_login_guest;
+
+    @FXML
+    private void minimize() {
+        Stage stage = (Stage) AP_ext.getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+    @FXML
+    private void quit() {
+        System.exit(0);
+    }
+
+    @FXML
+    private void darkStyleMinimize() {
+        setDarkHover(BT_minimize);
+    }
+
+    @FXML
+    private void darkStyleQuit() {
+        setDarkHover(BT_quit);
+    }
+
+    private void setDarkHover(ImageView iv) {
+        iv.setEffect(new InnerShadow(BlurType.GAUSSIAN, Color.rgb(0, 0, 0, 0.5), 10, 0, 5, 5));
+    }
+
+    @FXML
+    private void restoreStyleMinimize() {
+        resetDarkExit(BT_minimize);
+    }
+
+    @FXML
+    private void restoreStyleQuit() {
+        resetDarkExit(BT_quit);
+    }
+
+    private void resetDarkExit(ImageView iv) {
+        iv.setEffect(null);
+    }
+
+    @FXML
+    public void login(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void signUp(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void loginGuest(MouseEvent mouseEvent) {
+    }
 }
