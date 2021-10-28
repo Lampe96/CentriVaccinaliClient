88b453555cbdf6d4cdfba5cc8fa5cfe09af7a4ba
kuka6 <glatronico@studenti.uninsubria.it>
2021-10-28 19:16:59 +0200

    home hub pt2

--- src/main/java/org/project/hub/HubHomeController.java
+++ src/main/java/org/project/hub/HubHomeController.java
@@ -1,4 +1,27 @@
 package org.project.hub;
 
-public class HubHomeController {
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HubHomeController implements Initializable {
+
+    @FXML
+    public AnchorPane AP_ext;
+
+    private Stage stage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Platform.runLater(() -> {
+            stage = (Stage) AP_ext.getScene().getWindow();
+            stage.setMaxWidth(900);
+            stage.setMaxHeight(650);
+        });
+    }
 }
