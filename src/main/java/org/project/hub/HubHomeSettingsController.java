a0fa0a5a471aea7c624bed153dd879d8afa3c81f
kuka6 <glatronico@studenti.uninsubria.it>
2021-11-14 14:05:15 +0100

    piccole sistemazioni

--- src/main/java/org/project/hub/HubHomeSettingsController.java
+++ src/main/java/org/project/hub/HubHomeSettingsController.java
@@ -1,7 +1,6 @@
 package org.project.hub;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
-import io.github.palexdev.materialfx.controls.MFXLabel;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -17,16 +16,13 @@ import org.jetbrains.annotations.NotNull;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HubHomeSettingsController  implements Initializable {
+public class HubHomeSettingsController implements Initializable {
 
     @FXML
     public AnchorPane AP_ext;
 
     @FXML
     private ImageView BT_quit;
-
-    @FXML
-    private ImageView IV_zero;
 
     @FXML
     private ImageView IV_one;
@@ -50,7 +46,10 @@ public class HubHomeSettingsController  implements Initializable {
     private ImageView IV_seven;
 
     @FXML
-    private ImageView IV_eigth;
+    private ImageView IV_eight;
+
+    @FXML
+    private ImageView IV_nine;
 
     @FXML
     private MFXButton BT_delete;
@@ -70,7 +69,7 @@ public class HubHomeSettingsController  implements Initializable {
     @FXML
     private MFXButton BT_confirm_new_pwd;
 
-    private int selectedImage = 0;
+    private int selectedImage;
     private Stage stage;
 
     @Override
@@ -117,171 +116,203 @@ public class HubHomeSettingsController  implements Initializable {
     }
 
     @FXML
-    void darkStyleIVZero() {
-        setDarkHover(IV_zero);
-    }
-
-    @FXML
-    void darkStyleIVOne() {
+    private void darkStyleIVOne() {
         setDarkHover(IV_one);
     }
 
     @FXML
-    void darkStyleIVTwo() {
-        setDarkHover(IV_two);
-    }
-
-    @FXML
-    void darkStyleIVThree() {
-        setDarkHover(IV_three);
-    }
-
-    @FXML
-    void darkStyleIVFour() {
-        setDarkHover(IV_four);
-    }
-
-    @FXML
-    void darkStyleIVFive() {
-        setDarkHover(IV_five);
-    }
-
-    @FXML
-    void darkStyleIVSix() {
-        setDarkHover(IV_six);
-    }
-
-    @FXML
-    void darkStyleIVSeven() {
-        setDarkHover(IV_seven);
-    }
-
-    @FXML
-    void darkStyleIVEigth() {
-        setDarkHover(IV_eigth);
-    }
-
-    @FXML
-    void restoreStyleIVEigth() {
-        if (selectedImage != 8){
-            resetDarkExit(IV_eigth);
-        }
-    }
-
-    @FXML
-    void restoreStyleIVFive() {
-        if (selectedImage != 5){
-            resetDarkExit(IV_five);
-        }
-    }
-
-    @FXML
-    void restoreStyleIVFour() {
-        if (selectedImage != 4){
-            resetDarkExit(IV_four);
-        }
-    }
-
-    @FXML
-    void restoreStyleIVOne() {
-        if (selectedImage != 1){
+    private void restoreStyleIVOne() {
+        if (selectedImage != 1) {
             resetDarkExit(IV_one);
         }
     }
 
     @FXML
-    void restoreStyleIVSix() {
-        if (selectedImage != 6){
-            resetDarkExit(IV_six);
-        }
+    private void selectImageOne() {
+        setDarkHover(IV_one);
+        selectedImage = 1;
     }
 
     @FXML
-    void restoreStyleIVThree() {
-        if (selectedImage != 3){
-            resetDarkExit(IV_three);
-        }
+    private void darkStyleIVTwo() {
+        setDarkHover(IV_two);
     }
 
     @FXML
-    void restoreStyleIVTwo() {
-        if (selectedImage != 2){
+    private void restoreStyleIVTwo() {
+        if (selectedImage != 2) {
             resetDarkExit(IV_two);
         }
     }
 
     @FXML
-    void restoreStyleIVZero() {
-        if (selectedImage != 0){
-            resetDarkExit(IV_zero);
-        }
-    }
-
-    @FXML
-    void restoreStyleIVSeven() {
-        if (selectedImage != 7){
-            resetDarkExit(IV_seven);
-        }
-    }
-
-    @FXML
-    void selectImageEigth() {
-        setDarkHover(IV_eigth);
-        selectedImage = 8;
-    }
-
-    @FXML
-    void selectImageFive() {
-        setDarkHover(IV_five);
-        selectedImage = 5;
-    }
-
-    @FXML
-    void selectImageFour() {
-        setDarkHover(IV_four);
-        selectedImage = 4;
-    }
-
-    @FXML
-    void selectImageOne() {
-        setDarkHover(IV_one);
-        selectedImage = 0;
-    }
-
-    @FXML
-    void selectImageSeven() {
-        setDarkHover(IV_seven);
-        selectedImage = 7;
-    }
-
-    @FXML
-    void selectImageSix() {
-        setDarkHover(IV_six);
-        selectedImage = 6;
-    }
-
-    @FXML
-    void selectImageThree() {
-        setDarkHover(IV_three);
-        selectedImage = 3;
-    }
-
-    @FXML
-    void selectImageTwo() {
+    private void selectImageTwo() {
         setDarkHover(IV_two);
         selectedImage = 2;
     }
 
     @FXML
-    void selectImageZero() {
-        setDarkHover(IV_zero);
-        selectedImage = 0;
+    private void darkStyleIVThree() {
+        setDarkHover(IV_three);
+    }
+
+    @FXML
+    private void restoreStyleIVThree() {
+        if (selectedImage != 3) {
+            resetDarkExit(IV_three);
+        }
+    }
+
+    @FXML
+    private void selectImageThree() {
+        setDarkHover(IV_three);
+        selectedImage = 3;
+    }
+
+    @FXML
+    private void darkStyleIVFour() {
+        setDarkHover(IV_four);
+    }
+
+    @FXML
+    private void restoreStyleIVFour() {
+        if (selectedImage != 4) {
+            resetDarkExit(IV_four);
+        }
+    }
+
+    @FXML
+    private void selectImageFour() {
+        setDarkHover(IV_four);
+        selectedImage = 4;
+    }
+
+    @FXML
+    private void darkStyleIVFive() {
+        setDarkHover(IV_five);
+    }
+
+    @FXML
+    private void restoreStyleIVFive() {
+        if (selectedImage != 5) {
+            resetDarkExit(IV_five);
+        }
+    }
+
+    @FXML
+    private void selectImageFive() {
+        setDarkHover(IV_five);
+        selectedImage = 5;
+    }
+
+    @FXML
+    private void darkStyleIVSix() {
+        setDarkHover(IV_six);
+    }
+
+    @FXML
+    private void restoreStyleIVSix() {
+        if (selectedImage != 6) {
+            resetDarkExit(IV_six);
+        }
+    }
+
+    @FXML
+    private void selectImageSix() {
+        setDarkHover(IV_six);
+        selectedImage = 6;
+    }
+
+    @FXML
+    private void darkStyleIVSeven() {
+        setDarkHover(IV_seven);
+    }
+
+    @FXML
+    private void restoreStyleIVSeven() {
+        if (selectedImage != 7) {
+            resetDarkExit(IV_seven);
+        }
+    }
+
+    @FXML
+    private void selectImageSeven() {
+        setDarkHover(IV_seven);
+        selectedImage = 7;
+    }
+
+    @FXML
+    private void darkStyleIVEight() {
+        setDarkHover(IV_eight);
+    }
+
+    @FXML
+    private void restoreStyleIVEight() {
+        if (selectedImage != 8) {
+            resetDarkExit(IV_eight);
+        }
+    }
+
+    @FXML
+    private void selectImageEight() {
+        setDarkHover(IV_eight);
+        selectedImage = 8;
+    }
+
+    @FXML
+    private void darkStyleIVNine() {
+        setDarkHover(IV_nine);
+    }
+
+    @FXML
+    private void restoreStyleIVNine() {
+        if (selectedImage != 9) {
+            resetDarkExit(IV_nine);
+        }
+    }
+
+    @FXML
+    private void selectImageNine() {
+        setDarkHover(IV_nine);
+        selectedImage = 9;
     }
 
     private void resetStyle() {
         switch (selectedImage) {
-            case 0:
-                resetDarkExit(IV_zero);
+            case 1:
+                resetDarkExit(IV_one);
+                break;
+            case 2:
+                resetDarkExit(IV_two);
+                break;
+
+            case 3:
+                resetDarkExit(IV_three);
+                break;
+
+            case 4:
+                resetDarkExit(IV_four);
+                break;
+
+            case 5:
+                resetDarkExit(IV_five);
+                break;
+
+            case 6:
+                resetDarkExit(IV_six);
+                break;
+
+            case 7:
+                resetDarkExit(IV_seven);
+                break;
+
+            case 8:
+                resetDarkExit(IV_eight);
+                break;
+
+            case 9:
+                resetDarkExit(IV_nine);
+                break;
         }
     }
 }
