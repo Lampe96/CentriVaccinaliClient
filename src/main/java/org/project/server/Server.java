316f76d5d554b2e56991f4ff2fb8da50b10406a3
Lampe96 <lampe96@hotmail.it>
2021-11-28 20:31:18 +0100

    Ordinati tutti i metodi del server

--- src/main/java/org/project/server/Server.java
+++ src/main/java/org/project/server/Server.java
@@ -15,17 +15,13 @@ public interface Server extends Remote {
     String NAME = "SERVER";
     int PORT = 8000;
 
+    /**
+     * METODI LATO USER
+     */
+
     void insertDataUser(User user) throws RemoteException;
 
-    void insertDataHub(Hub hub) throws RemoteException;
-
-    void insertNewVaccinated(User vaccinatedUser) throws RemoteException;
-
     void changeDataUser(User vaccinatedUser) throws RemoteException;
-
-    void insertVaccinatedUserInNewHub(User vaccinatedUser) throws RemoteException;
-
-    void changeImage(int selectedImage, String hubName, String fiscalCode) throws RemoteException;
 
     boolean checkDuplicateNickname(String nick) throws RemoteException;
 
@@ -35,47 +31,65 @@ public interface Server extends Remote {
 
     boolean checkDuplicateFiscalCode(String fiscalCode) throws RemoteException;
 
-    boolean checkDuplicateHubName(String name) throws RemoteException;
-
-    boolean checkDuplicateAddress(String address) throws RemoteException;
-
-    UserType checkCredential(String email, String pwd) throws RemoteException;
-
-    boolean checkPassword(String hubName, String email, String pwd) throws RemoteException;
-
-    Object[] checkIfUserIsVaccinated(String hubName, String fiscalCode) throws RemoteException;
-
-    int checkIfFirstDose(String fiscalCode) throws RemoteException;
-
-    boolean checkIfHubExist(String hubName) throws RemoteException;
-
-    User fetchHubVaccinatedInfo(short UId, String hubName) throws RemoteException;
-
-    void changePwd(String hubName, String email, String newPwd) throws RemoteException;
-
-    void deleteAccount(String hubName, String email) throws RemoteException;
-
     void sendVerifyEmail(String email, String nickname) throws RemoteException;
 
     boolean verifyCodeEmail(String email, int code) throws RemoteException;
 
     void deleteReferenceVerifyEmail(String email) throws RemoteException;
 
-    ArrayList<User> fetchHubVaccinatedUser(String hubName) throws RemoteException;
-
     ArrayList<Hub> fetchAllHub() throws RemoteException;
-
-    ArrayList<AdverseEvent> fetchAllAdverseEvent(String hubName) throws RemoteException;
-
-    void updateVaccinatedUser(User vaccinatedUser) throws RemoteException;
-
-    boolean addAdverseEvent(AdverseEvent adverseEvent) throws RemoteException;
-
-    int[] getNumberVaccinated(String hubName) throws RemoteException;
 
     float getAvgAdverseEvent(String hubName) throws RemoteException;
 
-    User getUser(String email) throws RemoteException;
+    boolean addAdverseEvent(AdverseEvent adverseEvent) throws RemoteException;
 
     Hub getHub(String hubName) throws RemoteException;
+
+
+    /**
+     * METODI LATO HUB
+     */
+
+    boolean checkDuplicateAddress(String address) throws RemoteException;
+
+    void insertDataHub(Hub hub) throws RemoteException;
+
+    void insertNewVaccinated(User vaccinatedUser) throws RemoteException;
+
+    void insertVaccinatedUserInNewHub(User vaccinatedUser) throws RemoteException;
+
+    void updateVaccinatedUser(User vaccinatedUser) throws RemoteException;
+
+    boolean checkDuplicateHubName(String name) throws RemoteException;
+
+    Object[] checkIfUserIsVaccinated(String hubName, String fiscalCode) throws RemoteException;
+
+    boolean checkIfHubExist(String hubName) throws RemoteException;
+
+    int checkIfFirstDose(String fiscalCode) throws RemoteException;
+
+    ArrayList<User> fetchHubVaccinatedUser(String hubName) throws RemoteException;
+
+    User fetchHubVaccinatedInfo(short UId, String hubName) throws RemoteException;
+
+    User getUser(String email) throws RemoteException;
+
+
+    /**
+     * METODI CONDIVISI
+     */
+
+    void changeImage(int selectedImage, String hubName, String fiscalCode) throws RemoteException;
+
+    UserType checkCredential(String key, String pwd) throws RemoteException;
+
+    void changePwd(String hubName, String email, String newPwd) throws RemoteException;
+
+    boolean checkPassword(String hubName, String email, String pwd) throws RemoteException;
+
+    void deleteAccount(String hubName, String email) throws RemoteException;
+
+    ArrayList<AdverseEvent> fetchAllAdverseEvent(String hubName) throws RemoteException;
+
+    int[] getNumberVaccinated(String hubName) throws RemoteException;
 }
